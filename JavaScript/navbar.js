5e5574f28cc163816b089b9e0726a2c3beed68f7
Haseeb1604 <Haseeb200khan@gmail.com>
2021-03-18 22:00:14 +0500

    MSP-CUSIT demo

---
+++ JavaScript/navbar.js
@@ -0,0 +1,34 @@
+window.onload = () =>{
+    let navbar = document.getElementById("fixedNav")
+    let toggler = document.querySelector(".navbar-toggler")
+    let brand = document.querySelector('.brand-img')
+    let scroll = document.querySelector(".scroll-up-btn")
+
+    let sections = document.querySelectorAll("section")
+    let navlists = document.querySelectorAll(".nav-link")
+
+    window.onscroll = () =>{
+        if(window.pageYOffset > 0) {
+            navbar.classList.add("sticky")
+            scroll.classList.add("show")
+            brand.src = '../Assets/msp-cusit-white.svg'
+        }else{
+            navbar.classList.remove("sticky")
+            scroll.classList.remove("show")
+            brand.src = '../Assets/msp-cusit.svg'
+        }
+
+        let current = ''
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop
+            if(pageYOffset >= (sectionTop-700/3)) current = section.getAttribute("id")
+        })
+
+        navlists.forEach(li => {
+            li.classList.remove("active")
+            if(li.classList.contains(current)) li.classList.add("active")
+        })
+    }
+
+    toggler.addEventListener('click', () => toggler.classList.toggle('toggle'))
+}
